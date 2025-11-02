917f0d5c9bc36ebe188299a82f87b673c016b295
Donte Trumble <futurehitsdonte@aol.com>
2025-11-02 10:03:46 -0500

    adding home page

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,36 @@
         <v-app-bar :elevation="0" color="pink-darken-1" height="44" density="compact">
     </v-app-bar>
     <v-app-bar :elevation="2" color="white" height="84">
-        <v-app-bar-title class="font-weight-bold text-center text-uppercase text-white text-pink">Kanoee.Me</v-app-bar-title>
+        <v-app-bar-title class="font-weight-black text-center text-uppercase text-white text-pink">Kanoee.Me</v-app-bar-title>
+        <v-btn to="/" color="pink" variant="plain" class="hidden-sm-and-down">Home</v-btn>
+        <v-btn to="/about/" color="pink" variant="plain" class="hidden-sm-and-down">About</v-btn>
+        <v-menu class="hidden-md-and-up">
+            <template v-slot:activator="{ props }">
+              <v-btn icon="mdi-dots-vertical" variant="text" color="pink" v-bind="props" class="hidden-md-and-up"></v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item
+                v-for="(item, i) in navItem"
+                :key="i"
+                :value="i"
+                :to="item.to"
+              >
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
     </v-app-bar>
         <slot />
     </v-app>
 </div>
 </template>
+<script setup>
+    const navItem = ref([
+        { title: 'Home',
+            to: '/'
+         },
+    { title: 'About',
+            to: '/about/' },
+    ])
+</script>
---
+++ pages/about/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-main>
+        <v-container>
+            <h1 class="text-h4 text-pink font-weight-bold">About {{data[0].aboutTitle}}</h1>
+            <PortableText :value="data[0].aboutPage" class="text-body-1" #/>
+        </v-container>
+            
+    </v-main>
+</template>
+<script setup>
+import { PortableText } from '@portabletext/vue'
+    const params = useRoute().params
+    const queryG = groq `*[_type == "about"]`
+    const sanity = useSanity()
+    const {
+        data
+    } = await useAsyncData(`about`, () => sanity.fetch(queryG))
+    useHead({
+    title: `All things Kanoee`,
+    meta: [
+        { name: 'description', content: `Read everything about Kanoee.` },
+    ],
+    })
+</script>
