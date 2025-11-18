5d194bf0bda8235cfe917840a248fe3fe871cb1f
Donte Trumble <futurehitsdonte@aol.com>
2025-11-18 11:07:51 -0500

    ul styling v2

--- pages/wordoftheday/[id].vue
+++ pages/wordoftheday/[id].vue
@@ -4,19 +4,20 @@
             <v-container class="pt-0" v-if="data">
                 <!-- <pre>{{ data[0] }}</pre> -->
                  <v-card class="pa-3" flat>
-                    <section class="py-3">
+                    <section class="py-3 bg-pink rounded-lg elevation-3">
                         <v-row>
                             <v-col cols="7" md="9" sm="9">
                                 <v-breadcrumbs :items="breadcrumbsItems" class="py-0 pl-0 text-caption"></v-breadcrumbs>
                             </v-col>
                             <v-col cols="5" md="3" sm="3">
-                                <p class="text-right text-caption py-0">{{formatDate(data[0].dateWordPublished)}}</p>
+                                <p class="text-right text-caption py-0 pr-4">{{formatDate(data[0].dateWordPublished)}}</p>
                             </v-col>
                         </v-row>
                         
                     </section>
-                    <h1 class="text-h3 font-weight-black">{{ data[0].todayWordTitle }}</h1>
-                    <p class="text-subtitle-1 py-2 mb-0">Author {{data[0].authorOfThePost}}</p>
+                    <div class="mt-2 bg-pink-lighten-2 pa-5 rounded-lg elevation-2 ">
+                        <h1 class="text-h4 text-white font-weight-black">{{ data[0].todayWordTitle }}</h1>
+                    <p class="text-subtitle-2 py-2 mb-0">Author {{data[0].authorOfThePost}}</p>
                     <section id="social">
                         
                         <SocialShare
@@ -24,14 +25,19 @@
                         :key="network"
                         :network="network"
                         :label="false">
-                            <template #icon><v-btn :icon="`mdi-${network}`" flat></v-btn></template>
+                            <template #icon><v-btn :icon="`mdi-${network}`" color="white" flat variant="text"></v-btn></template>
                         </SocialShare>
                     </section>
+                    </div>
+                    <div class="pa-5 mt-2 rounded-lg bg-grey-lighten-4 elevation-1">
+                        
                 <v-card-text class="px-0">
                     <section id="main-story">
                     <PortableText :value="data[0].wordOfTheDay" class="text-body-1" #/>
                     </section>
                 </v-card-text>
+                    </div>
+                    
                 </v-card>
             </v-container>
         </v-main>
