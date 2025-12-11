6e96b222f892236d58809bb27fb5edebd706a369
Donte Trumble <futurehitsdonte@aol.com>
2025-12-11 00:24:45 -0500

    passage v2

--- pages/search-the-bible/passage/[id].vue
+++ pages/search-the-bible/passage/[id].vue
@@ -1,15 +1,17 @@
 <template>
     <v-main>
         <v-container v-if="bibleverusData">
-            <h1 class="text-h4 text-pink font-weight-bold">{{ bibleverusData.reference }}</h1>
-            <div class="bg-pink rounded-lg py-4 px-3">
-                <p class="text-body-2" style="line-height: 1;">Verse Count - {{ bibleverusData.verseCount }}</p>
+            <div class="py-3">
+                <h1 class="text-h4 text-pink font-weight-bold">{{ bibleverusData.reference }}</h1>
+                <p class="text-caption text-grey-darken-3" style="line-height: 1;">Chapter: {{bibleverusData.chapterIds[0]}} Verse Count - {{ bibleverusData.verseCount }}</p>
             </div>
+            
             
             <div class=" mt-2 rounded-lg bg-grey-lighten-5 elevation-1">
                 <div v-html="bibleverusData.content" class="px-6 py-4 text-body-1" id="main"></div>
             </div>
             <p class="text-grey pa-3 text-caption text-center">{{ bibleverusData.copyright }}</p>
+            
         </v-container>
         
     </v-main>
@@ -19,7 +21,7 @@
     const bibleverusData = ref()
     onMounted( async()=> {
         console.log(params)
-         const res = await $fetch(`https://rest.api.bible/v1/bibles/555fef9a6cb31151-01/passages/${params.id}?content-type=html&include-notes=false&include-titles=true&include-chapter-numbers=false&include-verse-numbers=true&include-verse-spans=false&use-org-id=false`, {
+         const res = await $fetch(`https://rest.api.bible/v1/bibles/555fef9a6cb31151-01/passages/${params.id}?content-type=html&include-notes=false&include-titles=true&include-chapter-numbers=true&include-verse-numbers=true&include-verse-spans=false&use-org-id=false`, {
             method: 'GET',
             headers: {
             'api-key' : `oKfPeujlWx8RUIwaQ_9-T`, // Use the API key securely
@@ -28,10 +30,6 @@
         })
         bibleverusData.value = res.data
     })
-   
-    //
-    //oKfPeujlWx8RUIwaQ_9-T
-    // const { data } = await useAsyncData('item', () => $fetch('https://rest.api.bible/v1/bibles/555fef9a6cb31151-01/books?include-chapters=false'))
 </script>
 <style>
     .v{
@@ -40,6 +38,12 @@
     }
     .v::after{
         content: ". ";
+    }
+    .c{
+        color: #353535
+    }
+    .c::before{
+        content: "Chapter ";
     }
     #main > p{ 
         color: #353535
